7aa8d5471afbda0de1680506ebe37b3c274381be
Auston <97974825+AKSRXX@users.noreply.github.com>
2023-04-12 15:21:46 -0500

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Employee extends Person 
+{
+    // constructor
+    public Employee(String name, String address, Date dob, String email, int ssn) 
+    {
+        super(name, address, dob, email, ssn);
+    }
+}
